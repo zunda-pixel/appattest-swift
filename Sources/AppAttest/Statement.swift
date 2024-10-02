70dcbba817d9cacd733b49df2a118a5b1b8b6ca7
zunda <47569369+zunda-pixel@users.noreply.github.com>
2024-10-02 19:02:04 +0900

    fix Statement

--- Sources/AppAttest/Statement.swift
+++ Sources/AppAttest/Statement.swift
@@ -2,39 +2,34 @@ import Foundation
 import SwiftASN1
 import X509
 
-public struct Statement: Decodable, Sendable, Hashable {
-  /// credCer
-  public var credetialCertificate: X509.Certificate
-  /// intermediateCa(caCert)
-  public var intermediateCertificateAuthority: X509.Certificate
-  public var receipt: ASN1Node
-
-  private enum CodingKeys: CodingKey {
-    case x5c
-    case receipt
-  }
-
-  public init(from decoder: any Decoder) throws {
-    let container = try decoder.container(keyedBy: CodingKeys.self)
-    let x5cs = try container.decode([Data].self, forKey: .x5c)
-    let certificates = try x5cs.map {
-      try X509.Certificate(derEncoded: Array($0))
-    }
-    if let credetialCertificate = certificates.first {
-      self.credetialCertificate = credetialCertificate
-    } else {
-      throw AppAttestError.missingCredetialCertificate
-    }
-
-    if let intermediateCertificateAuthority = certificates.last {
-      self.intermediateCertificateAuthority = intermediateCertificateAuthority
-    } else {
-      throw AppAttestError.missingIntermediateCertificateAuthority
+extension Attestation {
+  public struct Statement: Decodable, Sendable, Hashable {
+    /// credCer
+    public var credetialCertificate: X509.Certificate
+    /// intermediateCa(caCert)
+    public var intermediateCertificateAuthority: X509.Certificate
+    public var receipt: ASN1Node
+    
+    private enum CodingKeys: CodingKey {
+      case x5c
+      case receipt
     }
     
-    let receipt = try container.decode(Data.self, forKey: .receipt)
-    let receipt1 = try BER.parse(Array(receipt))
-
-    self.receipt = receipt1
+    public init(from decoder: any Decoder) throws {
+      let container = try decoder.container(keyedBy: CodingKeys.self)
+      let x5cs = try container.decode([Data].self, forKey: .x5c)
+      assert(x5cs.count == 2)
+      
+      let certificates = try x5cs.map {
+        try X509.Certificate(derEncoded: Array($0))
+      }
+      
+      self.credetialCertificate = certificates.first!
+      self.intermediateCertificateAuthority = certificates.last!
+      
+      let receipt = try container.decode(Data.self, forKey: .receipt)
+      let berReceipt = try BER.parse(Array(receipt))
+      self.receipt = berReceipt
+    }
   }
 }

4be74635297c42e4e79f3cd75227c046d7172b16
zunda <47569369+zunda-pixel@users.noreply.github.com>
2024-10-02 18:58:29 +0900

    add VerifyAssetion

---
+++ Sources/AppAttest/VerifyAssertion.swift
@@ -0,0 +1,54 @@
+import CryptoKit
+import Foundation
+import PotentCBOR
+import SwiftCBOR
+import X509
+
+extension AppAttest {
+  public func verifyAsssertion(
+    assertion: Data,
+    payload: Data,
+    certificate: X509.Certificate, // credentialCertificate from db attestation
+    counter: UInt32 // counter from db attestation
+  ) async throws {
+    let assertion = try CBORDecoder.default.decode(Assertion.self, from: assertion)
+    
+    if assertion.authenticatorData.counter <= counter {
+      throw AppAttestError.invalidCounter
+    }
+    
+    try verifyRelyingPartyId(
+      relyingPartyId: assertion.authenticatorData.relyingPartyId
+    )
+    
+    try Self.verifyNonce(
+      assetion: assertion,
+      payload: payload,
+      certificate: certificate
+    )
+  }
+  
+  static func verifyNonce(
+    assetion: Assertion, // Client
+    payload: Data, // Client
+    certificate: X509.Certificate // Server
+  ) throws {
+    let clientDataHash = Data(SHA256.hash(data: payload))
+    let nonceData: Data = assetion.authenticatorData.rawData + clientDataHash
+    let nonce = Data(SHA256.hash(data: nonceData))
+    let signature = try P256.Signing.ECDSASignature(derRepresentation: assetion.signature)
+    guard let publicKey = P256.Signing.PublicKey(certificate.publicKey) else {
+      throw AppAttestError.invalidPublicKey
+    }
+    if publicKey.isValidSignature(signature, for: nonce) == false {
+      throw AppAttestError.invalidNonce
+    }
+  }
+  
+  func verifyRelyingPartyId(relyingPartyId: Data) throws {
+    let appIdHash = Data(SHA256.hash(data: Data("\(self.teamId).\(self.bundleId)".utf8)))
+    if relyingPartyId != appIdHash {
+      throw AppAttestError.invalidRelyingPartyID
+    }
+  }
+}

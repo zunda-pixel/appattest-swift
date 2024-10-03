76ce742becfdba347f42fbb448ad8157789b68a5
zunda <47569369+zunda-pixel@users.noreply.github.com>
2024-10-04 01:18:59 +0900

    fix ContentView

--- Example/Clinet-App/AppAttestExample/ContentView.swift
+++ Example/Clinet-App/AppAttestExample/ContentView.swift
@@ -13,28 +13,42 @@ struct ContentView: View {
     let payload = Payload()
     
     let payloadData = try JSONEncoder().encode(payload)
-    let (data, _) = try await Client.execute(body: payloadData)
+    let (data, response) = try await Client.execute(body: payloadData)
     
-    self.response = String(decoding: data, as: UTF8.self)
+    if response.status == .ok {
+      self.response = String(decoding: data, as: UTF8.self)
+    }
   }
   
   var body: some View {
     List {
-      Button("Send Data") {
-        Task {
-          do {
-            try await sendData()
-          } catch {
-            self.error = error
+      Section {
+        Button("Send Data") {
+          Task {
+            do {
+              try await sendData()
+            } catch {
+              self.error = error
+            }
           }
         }
       }
       
       if let error {
-        Text(error.localizedDescription)
+        Section {
+          Text(error.localizedDescription)
+        }
       }
       if let response {
-        Text(response)
+        Section {
+          Text(response)
+        }
+      }
+      Section {
+        Button("Reset") {
+          error = nil
+          response = nil
+        }
       }
     }
   }

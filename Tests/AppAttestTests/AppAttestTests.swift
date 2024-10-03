fc96c94681eccf4a377bf43019df5f7583ce8023
zunda <47569369+zunda-pixel@users.noreply.github.com>
2024-10-03 22:49:29 +0900

    Add Test (#2)

--- Tests/AppAttestTests/AppAttestTests.swift
+++ Tests/AppAttestTests/AppAttestTests.swift
@@ -1,7 +1,92 @@
-import Testing
 import AppAttest
+import Crypto
+import DeviceCheck
+import Foundation
+import Testing
+
+struct Body: Codable {
+  let name: String
+  let age: Int
+  let challenge: Data
+  let keyId: String
+}
+
+func clientCode(challenge: Data) async throws -> (
+  attestation: Data,
+  assertion: Data,
+  bodyData: Data
+) {
+  let service = DCAppAttestService.shared
+  let keyId = try await service.generateKey()
+
+  let attestation = try await service.attestKey(
+    keyId,
+    clientDataHash: Data(SHA256.hash(data: challenge))
+  )
+
+  let body = Body(
+    name: "name1",
+    age: 25,
+    challenge: challenge,
+    keyId: keyId
+  )
+
+  let bodyData = try JSONEncoder().encode(body)
+  let assertion = try await service.generateAssertion(
+    keyId,
+    clientDataHash: Data(SHA256.hash(data: bodyData))
+  )
+
+  return (attestation, assertion, bodyData)
+}
+
+func serverCode(
+  attestation: Data,
+  assertion: Data,
+  bodyData: Data
+) async throws {
+  var teamId = Bundle.main.infoDictionary!["AppIdentifierPrefix"]! as! String  // "PU5HXZ4FZ2",
+  teamId.removeLast()
+
+  let bundleId = Bundle.main.bundleIdentifier!  //"com.zunda.TestAppAttest"
+
+  let appAttest = AppAttest(
+    teamId: teamId,
+    bundleId: bundleId
+  )
+
+  let body = try JSONDecoder().decode(Body.self, from: bodyData)
+
+  let attestatin = try await appAttest.verifyAttestation(
+    challenge: body.challenge,
+    keyId: Data(body.keyId.utf8),
+    attestation: attestation,
+    environment: .development
+  )
+
+  try appAttest.verifyAsssertion(
+    assertion: assertion,
+    payload: bodyData,
+    certificate: attestatin.statement.credetialCertificate,
+    counter: attestatin.authenticatorData.counter
+  )
+}
 
 @Test
-func hello() {
-  print("Hello World!")
+func appAttest() async throws {
+  #expect(DCAppAttestService.shared.isSupported)
+
+  // Generate Challenge Value On Server
+  let challeange = Data(AES.GCM.Nonce())
+  // Generate Attestation Assertion, BodyData on Client(iOS)
+  let (attestation, assertion, bodyData) = try await clientCode(challenge: challeange)
+  // Verify Data on Server
+  try await serverCode(
+    attestation: attestation,
+    assertion: assertion,
+    bodyData: bodyData
+  )
+  // User Body on Server
+  let body = try JSONDecoder().decode(Body.self, from: bodyData)
+  print(body)
 }
---
+++ Tests/AppAttestTests/DataAppAttestTests.swift
@@ -0,0 +1,52 @@
+import AppAttest
+import Foundation
+import Testing
+
+struct Request {
+  var teamId: String
+  var bundleId: String
+  var challenge: Data
+  var keyId: String
+  var attestation: Data
+  var assertion: Data
+  var bodyData: Data
+  
+  init(
+    teamId: String,
+    bundleId: String,
+    challenge: String,
+    keyId: String,
+    attestation: String,
+    assertion: String,
+    bodyData: String
+  ) {
+    self.teamId = teamId
+    self.bundleId = bundleId
+    self.challenge = Data(base64Encoded: challenge)!
+    self.keyId = keyId
+    self.attestation = Data(base64Encoded: attestation)!
+    self.assertion = Data(base64Encoded: assertion)!
+    self.bodyData = Data(base64Encoded: bodyData)!
+  }
+}
+
+func verifyAttestationAndAssersion(request: Request) async throws {
+  let appAttest = AppAttest(
+    teamId: request.teamId,
+    bundleId: request.bundleId
+  )
+  
+  let attestatin = try await appAttest.verifyAttestation(
+    challenge: request.challenge,
+    keyId: Data(request.keyId.utf8),
+    attestation: request.attestation,
+    environment: .development
+  )
+
+  try appAttest.verifyAsssertion(
+    assertion: request.assertion,
+    payload: request.bodyData,
+    certificate: attestatin.statement.credetialCertificate,
+    counter: attestatin.authenticatorData.counter
+  )
+}

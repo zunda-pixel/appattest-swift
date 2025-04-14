9334af5e6e8091b74176526aa18291b106edd76b
zunda.dev@gmail.com <zunda.dev@gmail.com>
2025-04-15 00:01:26 +0900

    fix Client

--- Example/AppAttest-Client-App/AppAttestExample/ContentView.swift
+++ Example/AppAttest-Client-App/AppAttestExample/ContentView.swift
@@ -1,33 +1,42 @@
 import SwiftUI
 
+struct User: Codable, Hashable {
+  var name: String
+  var age: Int
+}
+
 struct ContentView: View {
   @State var error: Error?
   @State var response: String?
-
+  @State var age: Int = 30
+  @State var users: [User] = []
+  
   func sendData() async throws {
-    struct Payload: Codable {
-      var name = "Hello World!"
-      var age = 42
-    }
-
-    let payload = Payload()
+    let payload = User(name: "Hello World!", age: age)
 
     let payloadData = try JSONEncoder().encode(payload)
-    let (data, response) = try await Client.execute(body: payloadData)
-
+    let (data, response) = try await Client.createUser(body: payloadData)
     if response.status == .ok {
+      print("Suceeded to send data!")
       self.response = String(decoding: data, as: UTF8.self)
+    } else {
+      print(response.status)
+      print(String(decoding: data, as: UTF8.self))
     }
   }
 
   var body: some View {
     List {
+      TextField("Age", value: $age, format: .number)
+        .keyboardType(.decimalPad)
+      
       Section {
-        Button("Send Data") {
+        Button("Create User") {
           Task {
             do {
               try await sendData()
             } catch {
+              print("Error: \(error)")
               self.error = error
             }
           }
@@ -41,13 +50,27 @@ struct ContentView: View {
       }
       if let response {
         Section {
-          Text(response)
+          Text("Created User \(response)")
         }
       }
       Section {
         Button("Reset") {
           error = nil
           response = nil
+        }
+      }
+      
+      Section {
+        Button("Fetch Users") {
+          Task {
+            self.users = try await Client.getUsers()
+          }
+        }
+      }
+      
+      Section("Users") {
+        ForEach(users, id: \.self) { user in
+          Text("\(user.name), \(user.age)")
         }
       }
     }

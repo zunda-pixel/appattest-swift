957e1e601e46842d72fe183b5aca7f23f33e9dd8
Yazawa Hiroki <47569369+zunda-pixel@users.noreply.github.com>
2026-05-11 10:35:09 +0900

    Remove App ID prefix compatibility shims

--- Sources/AppAttest/AppAttest.swift
+++ Sources/AppAttest/AppAttest.swift
@@ -8,12 +8,6 @@ public struct AppAttest: Sendable {
   public var bundleId: String
   public var environment: Environment
 
-  @available(*, deprecated, message: "Use appIDPrefix instead.")
-  public var teamId: String {
-    get { appIDPrefix }
-    set { appIDPrefix = newValue }
-  }
-
   public init(
     appIDPrefix: String,
     bundleId: String,
@@ -22,18 +16,5 @@ public struct AppAttest: Sendable {
     self.appIDPrefix = appIDPrefix
     self.bundleId = bundleId
     self.environment = environment
-  }
-
-  @available(*, deprecated, message: "Use init(appIDPrefix:bundleId:environment:) instead.")
-  public init(
-    teamId: String,
-    bundleId: String,
-    environment: Environment
-  ) {
-    self.init(
-      appIDPrefix: teamId,
-      bundleId: bundleId,
-      environment: environment
-    )
   }
 }

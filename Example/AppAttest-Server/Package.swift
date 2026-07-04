4c8693fb750a2bf66eb20351deae104b46324e68
zunda <47569369+zunda-pixel@users.noreply.github.com>
2026-07-04 21:33:09 +0900

    fix example (#72)

--- Example/AppAttest-Server/Package.swift
+++ Example/AppAttest-Server/Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 6.0
+// swift-tools-version: 6.3
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
   name: "AppAttest-Server",
   platforms: [
-    .macOS(.v14)
+    .macOS(.v26)
   ],
   dependencies: [
     .package(url: "https://github.com/hummingbird-project/hummingbird.git", from: "2.0.1"),
